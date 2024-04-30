0204aa06988196c1cb4380a32701f9f76c9c233d
root <root@Ubuntu.myguest.virtualbox.org>
2024-04-30 12:46:29 -0700

    New approach yet again to Ising model simulation"

--- simulation.py
+++ simulation.py
@@ -1,19 +1,59 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-N = 100  # Number of spins
-spins = np.random.choice([-1, 1], size=N)  # Initialize spins randomly
+def initialize_mps(N, d=2):
+    # Initialize random tensors for MPS
+    # Each tensor will have dimensions (d, chi, chi) where chi is the bond dimension
+    # For the edge tensors, the bond dimension on one side should be 1
+    chi = 4  # A small initial bond dimension
+    mps = [np.random.rand(d, 1, chi)]  # First tensor
+    for i in range(1, N - 1):
+        mps.append(np.random.rand(d, chi, chi))
+    mps.append(np.random.rand(d, chi, 1))  # Last tensor
+    return mps
 
-J = 1  # Interaction strength
+def construct_mpo(N, J, h):
+    # Constructing an MPO for the Ising model
+    # Here we need to define the MPO tensors that apply the Hamiltonian terms
+    sz = np.array([[1, 0], [0, -1]])  # Pauli Z
+    sx = np.array([[0, 1], [1, 0]])   # Pauli X
+    identity = np.eye(2)
+    mpo = []
+    # Use the Kronecker product to construct MPO tensors
+    for i in range(N):
+        mpo_tensor = np.zeros((2, 2, 4, 4))
+        mpo_tensor[:, :, 0, 0] = identity
+        mpo_tensor[:, :, 0, 1] = sz
+        mpo_tensor[:, :, 0, 2] = sx * h
+        mpo_tensor[:, :, 1, 3] = sz * J
+        mpo_tensor[:, :, 3, 3] = identity
+        mpo.append(mpo_tensor)
+    return mpo
 
-def ising_hamiltonian(spins, J):
-    return -J * np.sum(spins[:-1] * spins[1:])
+def optimize_tensor(tensor, neighbors, mpo):
+    # Placeholder for tensor optimization logic
+    return tensor
 
-energy = ising_hamiltonian(spins, J)
-print("Initial Energy of the system:", energy)
+def dmrg_sweep(mps, mpo, direction):
+    if direction == 'left_to_right':
+        for i in range(len(mps) - 1):
+            mps[i] = optimize_tensor(mps[i], (mps[i-1], mps[i+1]), mpo)
+    else:
+        for i in range(len(mps) - 1, 0, -1):
+            mps[i] = optimize_tensor(mps[i], (mps[i-1], mps[i+1]), mpo)
+    return mps
 
-plt.figure(figsize=(10, 2))
-plt.scatter(range(N), spins, c=spins, cmap='bwr', marker='_', lw=2)
-plt.ylim(-1.5, 1.5)
-plt.title('Initial Random Spin Configuration')
-plt.savefig('spin_configuration.png')
+# Parameters
+N = 100  # Number of sites
+J = 1    # Interaction strength
+h = 0.5  # External magnetic field
+
+# Initialize MPS and MPO
+mps = initialize_mps(N)
+mpo = construct_mpo(N, J, h)
+
+# Perform DMRG sweeps
+num_sweeps = 10
+for _ in range(num_sweeps):
+    mps = dmrg_sweep(mps, mpo, 'left_to_right')
+    mps = dmrg_sweep(mps, mpo, 'right_to_left')
+
